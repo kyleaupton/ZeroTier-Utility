a028eedc144e8dc3ffeaeb0cba2dddb7c64d94c0
Kyle Upton <kyleaupton@gmail.com>
2020-09-26 00:26:15 -0400

    Added some icons

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,16 @@ module.exports = {
     electronBuilder: {
       builderOptions: {
         publish: ["github"],
+        productName: "ZeroTier Utility",
+        appId: "com.electron.zerotier-utility",
+        mac: {
+          category: "public.app-category.utilities",
+          darkModeSupport: true,
+          icon: "build/zerotier-utility-app-icon.icns",
+        },
+        win: {
+          icon: "build/zerotier-utility-app-icon.ico",
+        },
       },
     },
   },
